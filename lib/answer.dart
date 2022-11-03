06e8ad0ba668bef97416439ab74a564b6cd2f804
noiro-id <25038088+noiro-id@users.noreply.github.com>
2022-11-03 15:56:46 +0700

    Finished chapter 46.

---
+++ lib/answer.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'package:legacy_buttons/LegacyRaisedButton.dart';
+
+class Answer extends StatelessWidget {
+  final Function selectHandler;
+  final String answerText;
+  const Answer(this.selectHandler, this.answerText, {super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: double.infinity,
+      child: LegacyRaisedButton(
+          onPressed: () => selectHandler(),
+          color: Colors.blue,
+          textColor: Colors.white,
+          child: Text(answerText)),
+    );
+  }
+}
