e061a799b54ff22327ba81b98057d6d11b769a60
noiro-id <25038088+noiro-id@users.noreply.github.com>
2022-11-03 15:56:46 +0700

    Finished chapter 46.

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,63 @@
 import 'package:flutter/material.dart';
+import 'package:legacy_buttons/LegacyRaisedButton.dart';
+import './question.dart';
+import './answer.dart';
 
 // void main() {
 //   runApp(MyApp());
 // }
 
-void main() => runApp(MyApp());
+void main() => runApp(const MyApp());
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
+  const MyApp({super.key});
+
+  @override
+  State<StatefulWidget> createState() {
+    // todolist: implement createState
+    return _MyAppState();
+    // throw UnimplementedError();
+  }
+}
+
+class _MyAppState extends State<MyApp> {
+  var _questionIndex = 0;
+
+  void _answerQuestion() {
+    setState(() {
+      _questionIndex = _questionIndex + 1;
+    });
+    print(_questionIndex);
+  }
+
   @override
   Widget build(BuildContext context) {
+    var questions = [
+      {
+        'questionText': 'What\'s your favorite color?',
+        'answers': ['Black', 'Red', 'Green', 'White']
+      },
+      {
+        'questionText': 'What\'s your favorite animal?',
+        'answers': ['Rabbit', 'Snake', 'Elephant', 'Lion']
+      },
+      {
+        'questionText': 'Who\'s your favorite instructor?',
+        'answers': ['Max', 'Max', 'Max', 'Max']
+      },
+    ];
     return MaterialApp(
-      home: Scaffold(
-          appBar: AppBar(
-            title: Text("My First App"),
-          ),
-          body: Text("This is my default text!")),
-    );
+        home: Scaffold(
+            appBar: AppBar(
+              title: const Text("My First App"),
+            ),
+            body: Column(children: [
+              Question(questions[_questionIndex]['questionText'] as String),
+              ...(questions[_questionIndex]['answers'] as List<String>)
+                  .map((answer) {
+                return Answer(_answerQuestion, answer);
+              })
+              // .toList()
+            ])));
   }
 }
